7d7181179245382d4b38e9a154801b865e4644c2
DaviGFiguer <davigarciafigueiredo@gmail.com>
2025-05-06 23:19:58 -0300

    Adição de CRUDS

--- src/main/java/com/MongoSpring/MongoSpring/Controller/MainController.java
+++ src/main/java/com/MongoSpring/MongoSpring/Controller/MainController.java
@@ -19,4 +19,33 @@ StudentRepo studentRepo;//Repository is used to store or insert or fetch data fo
 
     }
 
+    @GetMapping("/getStudent/{id}")
+    public Student getStudent(@PathVariable Integer id) { //will pick the Student body to make the request
+        return studentRepo.findById(id).orElse(null);
+
+    }
+
+    @GetMapping("/fetchStudents")
+    public List<Student> fetchStudents() { //will pick the Student body to make the request
+        return studentRepo.findAll();
+
+    }
+
+    @DeleteMapping("/deleteStudent/{id}")
+    public void deleteStudent(@PathVariable Integer id) {
+        studentRepo.deleteById(id);
+    }
+
+    @PutMapping("/updateStudent") //Here is posting data
+    public void updateStudent(@RequestBody Student student) { //will pick the Student body to make the request
+        Student data = studentRepo.findById(student.getRno()).orElse(null);
+
+        if(data != null) {
+            data.setName(student.getName());
+            data.setAddress(student.getAddress());
+            studentRepo.save(data);
+        }
+    }
+
+
 }
